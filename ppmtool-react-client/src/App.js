2620fcbc4f57ebc882e17bb57960424ec669b657
fabio-martin <fabiomartin2006@gmail.com>
2021-02-14 18:27:26 +0000

    added project, header components, bootstrap and style

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
-
+import Header from './components/Layout/Header';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard>
-
-      </Dashboard>
+      <Header />
+      <Dashboard />
     </div>
   );
 }
